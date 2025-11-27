098aed45ea76ea62562e102e87346c0c2465930a
umeshghimire111 <umeshghimire443@gmail.com>
2025-11-26 23:13:50 -0800

    create java project

---
+++ travelSewa/src/main/java/transportation/travelsewa/TravelsewaApplication.java
@@ -0,0 +1,12 @@
+package transportation.travelsewa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsewaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsewaApplication.class, args);
+	}
+}
---
+++ travelSewa/src/test/java/transportation/travelsewa/TravelsewaApplicationTests.java
@@ -0,0 +1,13 @@
+package transportation.travelsewa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsewaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
