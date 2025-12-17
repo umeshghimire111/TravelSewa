f99f606522a6b769e6f27592a3c8931288f4902a
umeshghimire111 <umeshghimire443@gmail.com>
2025-12-17 01:33:03 -0800

    Add and update SwaggerConfig

--- travelSewa/src/main/java/transportation/travelsewa/config/SwaggerConfig.java
+++ travelSewa/src/main/java/transportation/travelsewa/config/SwaggerConfig.java
@@ -1,4 +1,43 @@
 package transportation.travelsewa.config;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TravelSewa API")
+                       .description("API documentation for TravelSewa project")
+                ).servers(Arrays.asList(
+                        new Server().url("http://localhost:8085").description("local server"),
+                        new Server().url("http://localhost:8086").description("live server")))
+
+                .tags(Arrays.asList(
+                        new Tag().name("public APIs"),
+                        new Tag().name("User APIs"),
+                        new Tag().name("Admin APIs")
+                ))                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")
+                        ));
+    }
 }
