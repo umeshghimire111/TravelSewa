4c45e0087d058fa1ea78f8d055bbd31f88522e37
umeshghimire111 <umeshghimire443@gmail.com>
2026-01-01 17:41:50 +0545

    update qr scanner for showing user details after booking

--- travelSewa/src/main/java/transportation/travelsewa/qrScanner/ScannerController.java
+++ travelSewa/src/main/java/transportation/travelsewa/qrScanner/ScannerController.java
@@ -4,9 +4,9 @@ package transportation.travelsewa.qrScanner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/public")
     public class ScannerController {
-    @GetMapping("/public")
+    @GetMapping("/qr")
     public String publicPage() {
         return "QR Access Successful!";
     }
