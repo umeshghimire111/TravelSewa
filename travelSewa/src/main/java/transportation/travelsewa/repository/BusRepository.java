167366015e64c3ad5e7848fea5b5604a2ba8ce05
umeshghimire111 <umeshghimire443@gmail.com>
2026-01-01 17:38:51 +0545

    update bus reppository

--- travelSewa/src/main/java/transportation/travelsewa/repository/BusRepository.java
+++ travelSewa/src/main/java/transportation/travelsewa/repository/BusRepository.java
@@ -3,5 +3,9 @@ package transportation.travelsewa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import transportation.travelsewa.entity.Bus;
 
+import java.util.Optional;
+
 public interface BusRepository extends JpaRepository<Bus,Long> {
+    Optional<Bus> findByName(String busName);
+
 }
