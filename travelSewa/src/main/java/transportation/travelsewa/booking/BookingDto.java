f6c819f96ec7b1dbe057863cddf22bf31c2b833e
umeshghimire111 <umeshghimire443@gmail.com>
2026-01-01 17:33:33 +0545

    update BookingDTo

--- travelSewa/src/main/java/transportation/travelsewa/booking/BookingDto.java
+++ travelSewa/src/main/java/transportation/travelsewa/booking/BookingDto.java
@@ -1,19 +1,24 @@
 package transportation.travelsewa.booking;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
+import lombok.*;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class BookingDto {
 
-    @NotNull(message = "Bus ID is required")
-    private Long busId;
+    @NotNull(message="required bus name")
+    private String busName;
 
-    @NotNull(message = "Seats are required")
+    @NotNull(message = "Please specify how many seats you want to book")
+    @Min(value = 1, message = "You must book at least 1 seat")
+    @Max(value = 40, message = "You cannot book more than 40 seats at once")
     private Integer seats;
 
     @NotNull(message = "Travel date is required")
+    @FutureOrPresent(message = "Travel date must be today or next day")
     private LocalDate travelDate;
 }
